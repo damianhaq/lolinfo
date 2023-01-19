e567cb7a14d2b8529883c4a5732e6c6d104b927e
damianhaq <40294540+damianhaq@users.noreply.github.com>
2023-01-19 09:37:55 +0100

    champion card animations

--- src/components/champions/ChampionCard.js
+++ src/components/champions/ChampionCard.js
@@ -5,15 +5,17 @@ import { Link } from "react-router-dom";
 const ChampionCard = ({ data }) => {
   return (
     <Link to={`/champions/${data.id}`}>
-      <Box width={250} overflow={"hidden"} height={280} position={"relative"}>
+      <Box role={"group"} transitionDuration="0.3s" width={220} overflow={"hidden"} height={300} position={"relative"}>
         <Box
           position={"absolute"}
-          top={"-20px"}
+          top={"-25px"}
           left={"-20px"}
           width={308}
           height={560}
           backgroundPosition={"center"}
           backgroundImage={`url(http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${data.id}_0.jpg)`}
+          _groupHover={{ transform: "scale(0.97)" }}
+          transitionDuration="0.3s"
         ></Box>
         <Box
           position={"absolute"}
@@ -27,10 +29,22 @@ const ChampionCard = ({ data }) => {
           backgroundColor={"#061C25"}
           paddingLeft="19px"
           fontSize={"2xl"}
+          _groupHover={{ backgroundColor: "#006680", paddingLeft: "30px" }}
+          transitionDuration="0.3s"
         >
-          <p>{data.name}</p>
+          <Text>{data.name}</Text>
         </Box>
-        <Box top={-3} right={-3} position={"absolute"} transform={"rotate(45deg)"} width={6} height={6} backgroundColor={"white"}></Box>
+        <Box
+          top={-3}
+          right={-3}
+          transitionDuration="0.3s"
+          _groupHover={{ right: -6, top: -6 }}
+          position={"absolute"}
+          transform={"rotate(45deg)"}
+          width={6}
+          height={6}
+          backgroundColor={"white"}
+        ></Box>
       </Box>
     </Link>
   );
